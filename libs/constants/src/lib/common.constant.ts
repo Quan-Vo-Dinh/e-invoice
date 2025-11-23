f089c4353ef39779956dfa4b1b333010746dffe7
Quan-Vo-Dinh <vodinhquan2707.it@gmail.com>
2025-11-24 01:20:09 +0700

    feat(logger): implement request/response logging

--- libs/constants/src/lib/common.constant.ts
+++ libs/constants/src/lib/common.constant.ts
@@ -1,1 +1,4 @@
-export const PORT = 3000;
+export enum MetadataKey {
+  PROCESSID = 'processId',
+  STARTTIME = 'startTime',
+}
--- libs/middlewares/src/lib/logger.middlewares.ts
+++ libs/middlewares/src/lib/logger.middlewares.ts
@@ -1,15 +1,39 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { getProcessId } from '@common/utils/string.utils';
+import { MetadataKey } from '@common/constants/common.constant';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const startTime = Date.now();
     const { method, originalUrl, body } = req;
+    const processId = getProcessId('logger');
+    const now = new Date();
+
+    (req as any)[MetadataKey.PROCESSID] = processId;
+    (req as any)[MetadataKey.STARTTIME] = startTime;
 
     // dùng để log request đến server
     console.log(`[Request] ${method} ${originalUrl} - Body: ${JSON.stringify(body)}`);
-    const processId = process.pid;
+
+    Logger.log(
+      `HTTP >> Start ProcessId: '${processId}' >> method: '${method}' >> url: '${originalUrl}' >> at: '${now.toISOString()}' >> input: '${JSON.stringify(
+        body,
+      )}'`,
+    );
+
+    // khi express trả response về cho user thì nó sẽ gọi hàm res.send, vậy nên ta sẽ override hàm này để log response
+    const originalSend = res.send.bind(res);
+    res.send = (body?: any): Response => {
+      const responseTime = Date.now() - startTime;
+
+      Logger.log(
+        `HTTP << End ProcessId: '${processId}' << method: '${method}' << url: '${originalUrl}' << at: '${now.toISOString()}' << responseTime: '${responseTime} ms'`,
+      );
+
+      return originalSend(body);
+    };
     next();
   }
 }

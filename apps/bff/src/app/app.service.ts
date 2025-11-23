f58f3aa59794344cb8b48eacd50d9d59bf6d27ef
Quan-Vo-Dinh <vodinhquan2707.it@gmail.com>
2025-11-23 16:29:00 +0700

    feat(lib): create constants library and add PORT constant

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { PORT } from '@common/constants/common.constant';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    console.log(PORT);
     return { message: 'Hello API' };
   }
 }

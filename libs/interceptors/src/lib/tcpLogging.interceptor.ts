2428203abf2f73b083462fc5cea28fd658164570
Quan-Vo-Dinh <vodinhquan2707.it@gmail.com>
2025-12-03 13:14:58 +0700

    feat(invoice): add TCP logging interceptor

---
+++ libs/interceptors/src/lib/tcpLogging.interceptor.ts
@@ -0,0 +1,32 @@
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class TcpLoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    const now = Date.now();
+    const handler = context.getHandler();
+    const handlerName = handler.name || 'unknown_handler';
+
+    const args = context.getArgs();
+
+    const param = args[0];
+
+    const processId = param?.processId || 'unknown_process_id';
+
+    Logger.log(
+      `ProcessId: '${processId}' >> method: '${handlerName}' >> at '${now}' >> param: ${JSON.stringify(param)}`,
+      TcpLoggingInterceptor.name,
+    );
+
+    return next.handle().pipe(
+      tap(() => {
+        const duration = Date.now() - now;
+        Logger.log(
+          `ProcessId: '${processId}' >> method: '${handlerName}' >> completed in '${duration}ms'`,
+          TcpLoggingInterceptor.name,
+        );
+      }),
+    );
+  }
+}

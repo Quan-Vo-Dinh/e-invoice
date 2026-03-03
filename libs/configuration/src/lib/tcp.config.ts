b48d13c128e90e366749c5d8fb71d80002cdbbde
Vo Dinh Quan <vodinhquan2707.it@gmail.com>
2026-03-03 16:28:18 +0700

    feat(product): add new product application

--- libs/configuration/src/lib/tcp.config.ts
+++ libs/configuration/src/lib/tcp.config.ts
@@ -4,7 +4,7 @@ import { IsNotEmpty, IsObject } from 'class-validator';
 
 export enum TCP_SERVICES {
   INVOICE_SERVICE = 'TCP_INVOICE_SERVICE',
-  USER_SERVICE = 'TCP_USER_SERVICE',
+  PRODUCT_SERVICE = 'TCP_PRODUCT_SERVICE',
 }
 
 export class TcpConfiguration {
@@ -14,7 +14,7 @@ export class TcpConfiguration {
 
   @IsNotEmpty()
   @IsObject()
-  TCP_USER_SERVICE: TcpClientOptions;
+  TCP_PRODUCT_SERVICE: TcpClientOptions;
 
   constructor() {
     Object.entries(TCP_SERVICES).forEach(([key, serviceName]) => {
